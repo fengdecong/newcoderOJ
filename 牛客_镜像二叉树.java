23f8ca0258edefe576993ebf1c4af4054e2ba4bc
fengdecong <785608942@qq.com>
2020-08-27 17:26:58 +0800

    递归求镜像二叉树

--- 牛客_镜像二叉树.java
+++ 牛客_镜像二叉树.java
@@ -6,9 +6,9 @@ public void Mirror(TreeNode root){
 	TreeNode node = root.left;
 	root.left = root.right;
 	root.right = node;
-	while(root.left != null){
+	if(root.left != null){
 		Mirror(root.left);
-	}while(root.right != null){
+	}if(root.right != null){
 		Mirror(root.right);
 	} 
 }
